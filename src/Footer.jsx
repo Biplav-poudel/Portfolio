9f3197480ecd8b6046f4015a04e6231294f8641b
Biplav-poudel <pbiplav355@gmail.com>
2026-05-18 08:53:39 +0545

    Update social links and FAQ link in Footer

--- src/Footer.jsx
+++ src/Footer.jsx
@@ -4,14 +4,14 @@ const Footer = () => {
   return (
     <footer className="footer">
       <div className="social" data-aos="fade-down" data-aos-delay="500">
-        <a href="#"><i className='bx bxl-linkedin'></i></a>
-        <a href="#"><i className='bx bxl-github'></i></a>
-        <a href="#"><i className='bx bxl-instagram'></i></a>
-        <a href="#"><i className='bx bxl-facebook'></i></a>
+        <a href="https://www.linkedin.com/in/biplav-poudel-a688662bb/"><i className='bx bxl-linkedin'></i></a>
+        <a href="https://github.com/Biplav-poudel"><i className='bx bxl-github'></i></a>
+        <a href="https://www.instagram.com/biplavpoudel15/"><i className='bx bxl-instagram'></i></a>
+        <a href="https://www.facebook.com/biplavpoudel123"><i className='bx bxl-facebook'></i></a>
       </div>
       <div data-aos="fade-up" data-aos-delay="400">
         <ul className="list">
-          <li><a href="#">FAQ</a></li>
+          <li><a href="#home">FAQ</a></li>
           <li><a href="#service">Services</a></li>
           <li><a href="#about">About Me</a></li>
           <li><a href="#portfolio">Portfolio</a></li>
